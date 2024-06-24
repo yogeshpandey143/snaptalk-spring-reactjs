e933c19bcdf90ab8de922b74933342a229cca669
Yogesh Pandey <bc21143@smsvaranasi.in>
2024-06-24 21:09:28 +0530

    security

--- src/main/java/com/example/snaptalk/SnapTalk/Controller/UserController.java
+++ src/main/java/com/example/snaptalk/SnapTalk/Controller/UserController.java
@@ -29,20 +29,20 @@ public class UserController {
         return userService.regidterUser(user);
     }
 
-    @GetMapping("/user/{userId}")
+    @GetMapping("/api/user/{userId}")
     public User getUserById(@PathVariable("userId") Integer id){
 
         return userService.findUserById(id);
     }
 
-    @GetMapping("/user")
+    @GetMapping("/api/user")
     public List<User> getUsers(){
 
         List<User> users= userRepository.findAll();
        return users;
     }
 
-    @GetMapping("/user/email")
+    @GetMapping("/api/user/email")
     public User findByEmail(@RequestBody String email)
     {
 
@@ -50,14 +50,14 @@ public class UserController {
         return  user;
     }
 
-    @DeleteMapping("user/{userId}")
+    @DeleteMapping("/api/user/{userId}")
     public String deleteUser( @PathVariable("userId")Integer id)
     {
         return userService.deleteUser(id);
     }
 
 
-    @PutMapping("/user/{userId}")
+    @PutMapping("/api/user/{userId}")
     public User updateUser(@RequestBody User user ,@PathVariable Integer userId) throws Exception {
        User  myUser =userService.updateUser(user);
        if(myUser.getId()== userId) return  myUser;
@@ -67,13 +67,13 @@ public class UserController {
 
 
 
-    @PutMapping("user/follow/{userId1}/{userId2}")
+    @PutMapping("/api/user/follow/{userId1}/{userId2}")
     public User followUser(@PathVariable Integer userId1  , @PathVariable Integer userId2)
     {
       return userService.followUser(userId1,userId2);
     }
 
-    @GetMapping("user/search")
+    @GetMapping("/api/user/search")
     public List<User> searchUsers(@RequestParam("query") String query)
     {
         return userService.serachUser(query);
---
+++ src/main/java/com/example/snaptalk/SnapTalk/configsecutiry/Config.java
@@ -0,0 +1,28 @@
+package com.example.snaptalk.SnapTalk.configsecutiry;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Config {
+
+@Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+  http.sessionManagement(
+          management -> management.sessionCreationPolicy(
+                  SessionCreationPolicy.STATELESS)).authorizeHttpRequests(
+                          Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                                  .anyRequest().permitAll())
+          .csrf(csrf-> csrf.disable());
+
+  return http.build();
+
+}
+}
